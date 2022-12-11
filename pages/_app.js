bf3f4060470f1530d155694e06c985f88276dd3c
Kyle Shook <32422667+KyleShook@users.noreply.github.com>
2022-12-11 13:35:11 -0600

    NEW: Using next-seo vs normal meta tags (#7)

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,32 @@
 import "../styles/globals.css";
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				<title>Your dream job awaits</title>
-
-				<meta
-					property="og:url"
-					content="https://imaginary-opportunities.vercel.app"
-				/>
-				<meta property="og:type" content="website" />
-				<meta
-					property="og:title"
-					content="Welcome to Imaginary Opportunities"
-				/>
-				<meta
-					property="og:description"
-					content="Find exciting and unique job openings with your favorite fictional characters."
-				/>
-				<meta property="og:image" content="/images/metaImage.png" />
-
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta
-					property="twitter:domain"
-					content="https://imaginary-opportunities.vercel.app"
-				/>
-				<meta
-					property="twitter:url"
-					content="https://imaginary-opportunities.vercel.app"
-				/>
-				<meta
-					name="twitter:title"
-					content="Welcome to Imaginary Opportunities"
-				/>
-				<meta
-					name="twitter:description"
-					content="Find exciting and unique job openings with your favorite fictional characters."
-				/>
-				<meta name="twitter:image" content="/images/metaImage.png"></meta>
-			</Head>
+			<NextSeo
+				title={"Welcome to Imaginary Opportunities"}
+				titleTemplate={"Welcome to Imaginary Opportunities"}
+				defaultTitle={"Welcome to Imaginary Opportunities"}
+				description="Find exciting and unique job openings with your favorite fictional characters."
+				canonical="https://imaginary-opportunities.vercel.app/"
+				openGraph={{
+					url: "https://imaginary-opportunities.vercel.app/",
+					title: "Welcome to Imaginary Opportunities",
+					description:
+						"Find exciting and unique job openings with your favorite fictional characters.",
+					images: [
+						{
+							url: "https://imaginary-opportunities.vercel.app/images/metaImage.png",
+							width: 1200,
+							height: 630,
+						},
+					],
+				}}
+				twitter={{
+					cardType: "summary_large_image",
+				}}
+			/>
 			<Component {...pageProps} />
 		</>
 	);
