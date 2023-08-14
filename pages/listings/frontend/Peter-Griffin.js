25d8d5f343534a70a002ac84c1b69334d585b267
Mona Lisa <iamdheeraj2614@gmail.com>
2023-08-14 20:21:45 +0530

    Added new frontend developer job listing ( Peter Griffin )

---
+++ pages/listings/frontend/Peter-Griffin.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Listing = () => {
+	return (
+		<section className="listing">
+			<Link href="/">Return Home</Link>
+			<p>
+				<strong>Frontend Developer (Entry-Level)</strong>
+			</p>
+            <p>
+                Ready to code like it's going out of style, all while sharing a laugh with the one
+                and only Peter Griffin? At Griffin Web Creations, we're hunting for a frontend whiz
+                who can turn Peter's wild ideas into web reality. If you're a CSS, HTML, and JavaScript
+                aficionado, and your humor is as sharp as Peter's wit (well, almost), you're in for a treat!
+            </p>
+            <p>
+                Expect a workplace where impromptu chicken dances and meme-tastic design inspiration are the
+                norm. Your mission, should you choose to accept it, is to craft sites that scream "Freakin' 
+                Sweet!" while handling Peter's requests for buttons that shout "giggity!" And the perks? 
+                Think competitive salary, endless chuckles, and an annual BBQ bash that might just involve
+                tutus and coding challenges.
+            </p>
+            <p>
+                So, if you're itching to blend your code skills with Peter's brand of humor, shoot us your CV
+                and a joke that even Quagmire would high-five. Embrace the chaos, because at Griffin Web Creations,
+                we're coding for the giggles and web-ventures that'll keep you laughing all the way to payday! 🖥️🍔🤣
+            </p>
+		</section>
+	);
+};
+
+export default Listing;
